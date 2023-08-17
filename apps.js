4fb73879a1996a6064c0757c99ceb1ff1ab70b03
Mdforidulislam <mdforidulislam962@gmail.com>
2023-08-17 18:36:21 +0600

    update validtion or img

--- apps.js
+++ apps.js
@@ -1,96 +1,151 @@
 let productNumber = 1;
 
-document.getElementById('buynow-btn1').addEventListener('click',function () {
-    
-   const getTotalPreviusBalence = getElementValue('total-balence');
-   const getNewProductValue = getElementValue('product-price1');
-   const updatePrice = getTotalPreviusBalence + getNewProductValue;
-   upDateNewprice('total-balence',updatePrice)
-    
-   const updateProductNumber = productNumber ++ ;
+function buyProduct(btnId, productPriceElementId, cardContainerId, discountCodeElementId) {
+   const button = document.getElementById(btnId);
+   const limit = 15;
+   let purchasedQuantity = 1;
+   let balenceLimitation = 4000;
+   button.addEventListener('click', function() {
 
-   upDateNewprice('product-number',updateProductNumber);
-   upDateNewprice('item-set',updateProductNumber);
+        const getTotalPreviusBalence = getElementValue('total-balence');
+        const getNewProductValue = getElementValue(productPriceElementId);
+        const updatePrice = getTotalPreviusBalence + getNewProductValue;
 
-   addproductInCard("carad-container",getNewProductValue,updateProductNumber);
+      if (purchasedQuantity <= limit && updatePrice <= balenceLimitation ) {
+         // Proceed with purchase
+           const getTotalPreviusBalence = getElementValue('total-balence');
+           const getNewProductValue = getElementValue(productPriceElementId);
+           const updatePrice = getTotalPreviusBalence + getNewProductValue;
+           upDateNewprice('total-balence', updatePrice);
 
+           const updateProductNumber = productNumber++;
+           addproductInCard(cardContainerId, getNewProductValue, updateProductNumber, productPriceElementId);
+           upDateNewprice('product-number', updateProductNumber);
+           upDateNewprice('item-set', updateProductNumber);
 
-})
-document.getElementById('buynow-btn2').addEventListener('click',function () {
-    
-   const getTotalPreviusBalence = getElementValue('total-balence');
-   const getNewProductValue = getElementValue('product-price1');
-   const updatePrice = getTotalPreviusBalence + getNewProductValue;
-   upDateNewprice('total-balence',updatePrice)
-    
-   const updateProductNumber = productNumber ++ ;
-   addproductInCard("carad-container",getNewProductValue,updateProductNumber);
-   upDateNewprice('product-number',updateProductNumber);
-   upDateNewprice('item-set',updateProductNumber);
+           if (discountCodeElementId) {
+               const getTotalPreviusBalence = getElementValue('total-balence');
+               const getNewProductValue = getElementValue(productPriceElementId);
+               const setPriceDiscount = discountProduct(discountCodeElementId, getNewProductValue, updateProductNumber, getTotalPreviusBalence);
+           }
 
-})
-document.getElementById('buynow-btn3').addEventListener('click',function () {
-    
-   const getTotalPreviusBalence = getElementValue('total-balence');
-   const getNewProductValue = getElementValue('product-price1');
-   const updatePrice = getTotalPreviusBalence + getNewProductValue;
-   upDateNewprice('total-balence',updatePrice)
-    
-   const updateProductNumber = productNumber ++ ;
-   addproductInCard("carad-container",getNewProductValue,updateProductNumber);
-   upDateNewprice('product-number',updateProductNumber);
-   upDateNewprice('item-set',updateProductNumber);
+           purchasedQuantity++;
 
-})
-document.getElementById('buynow-btn4').addEventListener('click',function () {
-    
-   const getTotalPreviusBalence = getElementValue('total-balence');
-   const getNewProductValue = getElementValue('product-price1');
-   const updatePrice = getTotalPreviusBalence + getNewProductValue;
-   upDateNewprice('total-balence',updatePrice)
-   const updateProductNumber = productNumber ++ ;
-   addproductInCard("carad-container",getNewProductValue,updateProductNumber);
+           // Display a message if the purchase limit is reached
+           if (purchasedQuantity === limit) {
+               alert(`You have reached the purchase limit of ${limit} for this product.`);
+           }
+       } else {
+           alert(`You have reached the purchase limit of ${limit} for this product.`);
+       }
+   });
+}
 
-   upDateNewprice('product-number',updateProductNumber);
-   upDateNewprice('item-set',updateProductNumber);
-
-})
-document.getElementById('buynow-btn5').addEventListener('click',function () {
-    
-   const getTotalPreviusBalence = getElementValue('total-balence');
-   const getNewProductValue = getElementValue('product-price1');
-   const updatePrice = getTotalPreviusBalence + getNewProductValue;
-   upDateNewprice('total-balence',updatePrice)
-    
-   const updateProductNumber = productNumber ++ ;
-   addproductInCard("carad-container",getNewProductValue,updateProductNumber);
-   upDateNewprice('product-number',updateProductNumber);
-   upDateNewprice('item-set',updateProductNumber);
-
-})
-document.getElementById('buynow-btn6').addEventListener('click',function () {
-    
-   const getTotalPreviusBalence = getElementValue('total-balence');
-   const getNewProductValue = getElementValue('product-price1');
-   const updatePrice = getTotalPreviusBalence + getNewProductValue;
-   upDateNewprice('total-balence',updatePrice)
-    
-   const updateProductNumber = productNumber ++ ;
-   addproductInCard("carad-container",getNewProductValue,updateProductNumber);
-   upDateNewprice('product-number',updateProductNumber);
-   upDateNewprice('item-set',updateProductNumber);
-
-})
+buyProduct('buynow-btn1', 'product-price1', 'carad-container', 'Discount-code');
+buyProduct('buynow-btn2', 'product-price2', 'carad-container');
+buyProduct('buynow-btn3', 'product-price3', 'carad-container');
+buyProduct('buynow-btn4', 'product-price4', 'carad-container');
+buyProduct('buynow-btn5', 'product-price5', 'carad-container');
+buyProduct('buynow-btn6', 'product-price6', 'carad-container');
 
 document.getElementById('disocunt-offer').addEventListener('click', function () {
    const updateProductNumber = productNumber++; // Assuming you have defined productNumber
    const getNewProductValue = getElementValue('product-price1'); // Assuming you have the getElementValue function
    const getTotalPreviusBalence = getElementValue('total-balence'); // Assuming you have the getElementValue function
-   const setPriceDiscount = discountProduct('Discount-code', getNewProductValue, updateProductNumber,getTotalPreviusBalence);
+   const setPriceDiscount = discountProduct('Discount-code', getNewProductValue, updateProductNumber, getTotalPreviusBalence);
 });
 
 
 
 
 
+// old code =====================================================================================
 
+// document.getElementById('buynow-btn1').addEventListener('click',function () {
+    
+//    const getTotalPreviusBalence = getElementValue('total-balence');
+//    const getNewProductValue = getElementValue('product-price1');
+//    const updatePrice = getTotalPreviusBalence + getNewProductValue;
+//    upDateNewprice('total-balence',updatePrice)
+    
+//    const updateProductNumber = productNumber ++ ;
+
+//    upDateNewprice('product-number',updateProductNumber);
+//    upDateNewprice('item-set',updateProductNumber);
+
+//    addproductInCard("carad-container",getNewProductValue,updateProductNumber,"product-price1");
+
+
+// })
+// document.getElementById('buynow-btn2').addEventListener('click',function () {
+    
+//    const getTotalPreviusBalence = getElementValue('total-balence');
+//    const getNewProductValue = getElementValue('product-price2');
+//    const updatePrice = getTotalPreviusBalence + getNewProductValue;
+//    upDateNewprice('total-balence',updatePrice)
+    
+//    const updateProductNumber = productNumber ++ ;
+//    addproductInCard("carad-container",getNewProductValue,updateProductNumber,'product-price2');
+//    upDateNewprice('product-number',updateProductNumber);
+//    upDateNewprice('item-set',updateProductNumber);
+
+// })
+// document.getElementById('buynow-btn3').addEventListener('click',function () {
+    
+//    const getTotalPreviusBalence = getElementValue('total-balence');
+//    const getNewProductValue = getElementValue('product-price3');
+//    const updatePrice = getTotalPreviusBalence + getNewProductValue;
+//    upDateNewprice('total-balence',updatePrice)
+    
+//    const updateProductNumber = productNumber ++ ;
+//    addproductInCard("carad-container",getNewProductValue,updateProductNumber,'product-price3');
+//    upDateNewprice('product-number',updateProductNumber);
+//    upDateNewprice('item-set',updateProductNumber);
+
+// })
+// document.getElementById('buynow-btn4').addEventListener('click',function () {
+    
+//    const getTotalPreviusBalence = getElementValue('total-balence');
+//    const getNewProductValue = getElementValue('product-price4');
+//    const updatePrice = getTotalPreviusBalence + getNewProductValue;
+//    upDateNewprice('total-balence',updatePrice)
+//    const updateProductNumber = productNumber ++ ;
+//    addproductInCard("carad-container",getNewProductValue,updateProductNumber,'product-price4');
+
+//    upDateNewprice('product-number',updateProductNumber);
+//    upDateNewprice('item-set',updateProductNumber);
+
+// })
+// document.getElementById('buynow-btn5').addEventListener('click',function () {
+    
+//    const getTotalPreviusBalence = getElementValue('total-balence');
+//    const getNewProductValue = getElementValue('product-price5');
+//    const updatePrice = getTotalPreviusBalence + getNewProductValue;
+//    upDateNewprice('total-balence',updatePrice)
+    
+//    const updateProductNumber = productNumber ++ ;
+//    addproductInCard("carad-container",getNewProductValue,updateProductNumber,'product-price5');
+//    upDateNewprice('product-number',updateProductNumber);
+//    upDateNewprice('item-set',updateProductNumber);
+
+// })
+// document.getElementById('buynow-btn6').addEventListener('click',function () {
+    
+//    const getTotalPreviusBalence = getElementValue('total-balence');
+//    const getNewProductValue = getElementValue('product-price6');
+//    const updatePrice = getTotalPreviusBalence + getNewProductValue;
+//    upDateNewprice('total-balence',updatePrice)
+    
+//    const updateProductNumber = productNumber ++ ;
+//    addproductInCard("carad-container",getNewProductValue,updateProductNumber,'product-price6');
+//    upDateNewprice('product-number',updateProductNumber);
+//    upDateNewprice('item-set',updateProductNumber);
+
+// })
+
+// document.getElementById('disocunt-offer').addEventListener('click', function () {
+//    const updateProductNumber = productNumber++; // Assuming you have defined productNumber
+//    const getNewProductValue = getElementValue('product-price1'); // Assuming you have the getElementValue function
+//    const getTotalPreviusBalence = getElementValue('total-balence'); // Assuming you have the getElementValue function
+//    const setPriceDiscount = discountProduct('Discount-code', getNewProductValue, updateProductNumber,getTotalPreviusBalence);
+// });
--- function.js
+++ function.js
@@ -9,20 +9,43 @@ function upDateNewprice (elementId,price){
      getSetPriceValue.innerText = price;
 }
 
-function addproductInCard(cardId,ProductPrice,QuantityProduct) {
+function addproductInCard(cardId, ProductPrice, QuantityProduct,imgTitleshow) {
+   const getProdcuPrice = document.getElementById(imgTitleshow);
+   const getprodcutParent = getProdcuPrice.parentNode.parentNode.parentNode.parentNode;
+   const getProductFastChild = getprodcutParent.childNodes[1].firstChild;
+   const getProductFastChildContent = getProductFastChild.outerHTML;
+   const getProductTitle = getprodcutParent.children[1].firstElementChild.innerText;
    const getCardSection = document.getElementById(cardId);
    const createPriceTitle = document.createElement('h2');
    const createProductQuantity = document.createElement('h2');
    const creatSum = document.createElement('h1');
-   const multipulation = ProductPrice *QuantityProduct ;
-   createProductQuantity.innerText = "Product Quantity : " + QuantityProduct;
-   createPriceTitle.innerText ="Product Price $ : " + ProductPrice;
-   creatSum.innerText = "Total Price : $ " + multipulation ;
+   const multipulation = ProductPrice * QuantityProduct;
 
-   getCardSection.appendChild(createPriceTitle);
-   getCardSection.appendChild(createProductQuantity);
-   getCardSection.appendChild(creatSum);
+   createProductQuantity.innerHTML = getProductFastChildContent + getProductTitle + QuantityProduct;
+   createPriceTitle.innerText = "Product Price $: " + ProductPrice;
+   creatSum.innerText = "Total Price: $" + multipulation;
+
+   const createDivP = document.createElement('div');
+   createDivP.style.cssText = 'display: flex; justify-content: space-between; background-color: #0b2942; padding: 12px; border-radius:12px; margin-top:5px;';
+   createProductQuantity.style.width = '100%';
+
+   const createDivC1 = document.createElement('div');
+   const createDivC2 = document.createElement('div');
+   const createDivC3 = document.createElement('div');
+
+   createDivC1.appendChild(createProductQuantity);
+   createDivC2.appendChild(createPriceTitle);
+   createDivC3.appendChild(creatSum);
+
+   createDivC1.style.width = '100px';
+
+   createDivP.appendChild(createDivC1);
+   createDivP.appendChild(createDivC2);
+   createDivP.appendChild(createDivC3);
+
+   getCardSection.appendChild(createDivP);
 }
+
 
 function discountProduct(discountcode, productPrice, quantity,totalPrice) {
    const discountValue = document.getElementById(discountcode).value;
@@ -34,11 +57,11 @@ function discountProduct(discountcode, productPrice, quantity,totalPrice) {
        const getTrim = getChild.textContent.trim();
        const getNumber = getTrim.split(' ');
        const getValue = parseFloat(getNumber[4]);
-       
+       console.log(getValue)
        const discount = (60 / 100) * productPrice;
        const discountPrice = productPrice - discount;
        const totalQuantityPrice = quantity * discountPrice;
-       const remainBalce = totalPrice - totalQuantityPrice;
+       const remainBalce =totalPrice - totalQuantityPrice;
 
        const createElement = document.createElement('h1');
        createElement.innerText ="Discount Price : " + totalQuantityPrice;
